ce1924c9acf143b49f0f77936ccef4f77f17cf48
박유경 <pyk0514@kumoh.ac.kr>
2022-08-15 22:59:23 +0900

    [try] 1966
    우선순위큐 공부 후 적용해볼 예정

--- python/class2/boj1966.py
+++ python/class2/boj1966.py
@@ -5,16 +5,22 @@ t = int(input())
 for _ in range(t):
     n, m = map(int, sys.stdin.readline().rstrip().split())
     focus = deque(list(map(int, sys.stdin.readline().rstrip().split())))
-    
-    while focus[m] != max(focus):
-        if focus[0] == max(focus):
-            focus.popleft()
-        else:
+    target = deque([ False for _ in range(len(focus))])
+    target[m] = True
+
+    while focus != deque(sorted(focus, reverse=True)) and target[0]:
+        maxval = max(focus)
+        top = focus.popleft()
+        if top != maxval:
+            focus.append(top)
             focus.rotate(1)
+            target.rotate(1)
+            if m == 0:
+                m = len(focus)-1
+            else:
+                m -= 1
 
-        if m <= 0:
-            m = len(focus)-1
-        else:
-            m -= 1
-
-    print(m+1)
+    if(m == 0):
+        print(1)
+    else:
+        print(m)
